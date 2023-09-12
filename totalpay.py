3d762082128cacc1df3b5fca34aec87f7d763e5b
SolipsesDualboot <144247870+SolipsesDualboot@users.noreply.github.com>
2023-09-11 22:50:36 -0300

    Organized Comments for better readability

--- totalpay.py
+++ totalpay.py
@@ -7,11 +7,12 @@ def input_test(currencies, switch):  # The two variable names between the () ind
     if not switch:  # Switch is intended to be a boolean. this checks if switch is set to false
         currencies[0] = float(input("what is your weekly salary? "))  # Lists start at index 0, like binary
     else:
-        currencies[1] = float(input("what is your bonus? "))
+        currencies[1] = float(input("what is your bonus? "))  # Must not be a plain integer, they are immutable
 
 
 def pay_pan():
-    currencies = list((None, None))  # We intend to store two numbers in this list and no more, though You could maybe alternatively use .append() in input_test on an empty list
+    currencies = list((None, None))  # We intend to store two numbers in this list and no more
+    # though You could maybe alternatively use .append() in input_test on an empty list
     while True:  # Forms an infinite loop until this is done correctly
         try:    # First part of error handling
             input_test(currencies, False)
@@ -26,7 +27,8 @@ def pay_pan():
                 break
         except ValueError:
             print("Only enter a number!")
-    total_pay = str(currencies[0] + currencies[1])  # It is fine to assume the values are integers as we already made sure
+    total_pay = str(currencies[0] + currencies[1])  # It is fine to assume the values are integers
+    # as we already checked when setting them
     print("the total pay for this week is $" + total_pay)
 
 
