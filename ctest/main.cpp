f60eda79d50f25747296b7a41a6ed58c769a5377
SolipsesDualboot <144247870+SolipsesDualboot@users.noreply.github.com>
2023-09-13 08:51:11 -0300

    C++ Sum of two numbers test

---
+++ ctest/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "sumoftwo.h"
+
+int wee = 0;
+int woo = 0;
+bool wuu;
+
+int main()
+{
+    //while(wee = 0)
+        //std::cin >> wee;
+        //if(std::cin.fail())
+        //{
+            //wee = 1;
+        //}
+    std::cin >> wee;
+    std::cin >> woo;
+    sumoftwo(wee, woo, wuu);
+    std::cout << wee;
+}
---
+++ ctest/sumoftwo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+void sumoftwo(int &int0, int &int1, bool isswitch)
+{
+    //if(isswitch = false)
+    //{
+    int0 = int0 + int1;
+    //std::cout << int0;
+    //};
+}
